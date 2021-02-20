a639ed31aac69b3062668ae4ceb37eabdc8a5513
Zeke <51855202+zyrrus@users.noreply.github.com>
2021-02-20 01:47:53 -0600

    Add files via upload

---
+++ board.js
@@ -0,0 +1,175 @@
+class Board {
+    constructor() {
+        this.highlighting = [];
+        this.selected = [];
+        this.arrows = [];
+
+        this.p1Turn = true;
+
+        // this.board = [
+        //     [2, 2, 2, 2, 2, 2, 2, 2, 2], 
+        //     [2, 2, 2, 2, 2, 2, 2, 2, 2],
+        //     [2, 1, 2, 1, 0, 2, 1, 2, 1], 
+        //     [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        //     [1, 1, 1, 1, 1, 1, 1, 1, 1]
+        // ];
+
+        this.board = [
+            [0, 0, 1, 0, 0, 0, 0, 0, 0], 
+            [0, 0, 0, 0, 0, 0, 2, 0, 0],
+            [0, 2, 0, 2, 1, 0, 2, 0, 0], 
+            [0, 0, 0, 0, 0, 0, 2, 0, 0],
+            [0, 0, 0, 0, 2, 0, 0, 0, 0]
+        ];
+    }
+
+    render() {
+        this.drawBG();
+
+        for (let r = 0; r < this.board.length; r++) {
+            for (let c = 0; c < this.board[0].length; c++) {
+                if (this.board[r][c] == 1) { this.drawBoldCircle(r, c, beige); }
+                else if (this.board[r][c] == 2) { this.drawBoldCircle(r, c, green); }
+            }
+        }
+
+        if (this.selected.length > 0) {
+            this.drawBoldCircle(this.selected[0], this.selected[1], black);
+        }
+
+        this.arrows.forEach(arrow => {
+            this.drawArrow(arrow[0], arrow[1], arrow[2], arrow[3]);
+        });
+
+        this.highlight();
+    }
+    
+    clickedOn(r, c) {
+        if ((0 <= r && r < this.board.length) && (0 <= c && c < this.board[0].length)) return this.board[r][c];
+        return -1;
+    }
+
+    highlight() {
+        this.highlighting.forEach(spot => {
+            noStroke();
+            fill(255, 255, 255, 150);
+            this.drawCircle(spot[0], spot[1]);
+        });
+    }
+
+    addHighlight(coord) {
+        this.highlighting.push(coord);
+    }
+
+    addArrow(row1, col1, row2, col2) {
+        this.arrows.push([row1, col1, row2, col2]);
+    }
+
+    drawArrow(row1, col1, row2, col2) {
+        let tail = RCtoXY(row1, col1);
+        let tip = RCtoXY(row2, col2);
+        let dir = getDirection(row1, col1, row2, col2);
+        let midpoint = [(tip[0] + tail[0])/2, (tip[1] + tail[1])/2];
+        let size = scale / 5;
+
+        dir = [dir[1], dir[0]];
+        let compass = ""; 
+        let angle;
+
+        // Determine the direction of movement
+        switch (dir[1]) {
+            case 1: 
+                compass += "N";
+                break;
+            case -1: 
+                compass += "S";
+                break; 
+        }
+        switch (dir[0]) {
+            case 1: 
+                compass += "E";
+                break;
+            case -1: 
+                compass += "W";
+                break; 
+        }
+        
+        // Determine the angle
+        switch (compass) {
+            case "E": 
+                angle = 0;
+                break;
+            case "NE": 
+                angle = PI/4;
+                break;
+            case "N": 
+                angle = PI/2;
+                break;
+            case "NW": 
+                angle = 3 * PI/4;
+                break;
+            case "W": 
+                angle = PI;
+                break;
+            case "SW": 
+                angle = 5 * PI/4;
+                break;
+            case "W": 
+                angle = 3 * PI/2;
+                break;
+            case "SE": 
+                angle = 7 * PI/4;
+                break;
+        }
+        angle += PI/2;
+        translate(midpoint[0], midpoint[1]);
+        rotate(angle);
+        fill(black);
+        triangle(0, -size/2, -size/2, size, size/2, size);
+        
+        rotate(-angle);
+        translate(-midpoint[0], -midpoint[1]);
+        
+    }
+
+    drawBoldCircle(r, c, color) {
+        stroke(black);
+        strokeWeight(scale);
+        fill(color);
+        this.drawCircle(r, c);
+    }
+
+    drawCircle(r, c) {
+        let xy = RCtoXY(r, c);
+        circle(xy[0], xy[1], 3 * scale);
+    }
+    // Draw the background lines
+    drawBG() {
+        stroke(black);
+        strokeWeight(4);
+
+        // Horizontals ------------------------------------------
+        for (let i = 0; i < 5; i++) { this.drawLine(i, 0, i, 8); }
+        // Verticals --------------------------------------------
+        for (let i = 0; i < 9; i++) { this.drawLine(0, i, 4, i); }
+        // Diagonals --------------------------------------------
+        //     Negative slope
+        this.drawLine(2, 0, 4, 2);
+        this.drawLine(0, 0, 4, 4);
+        this.drawLine(0, 2, 4, 6);
+        this.drawLine(0, 4, 4, 8);
+        this.drawLine(0, 6, 2, 8);
+        //     Positive slope 
+        this.drawLine(2, 0, 0, 2);
+        this.drawLine(4, 0, 0, 4);
+        this.drawLine(4, 2, 0, 6);
+        this.drawLine(4, 4, 0, 8);
+        this.drawLine(4, 6, 2, 8);
+    }
+
+    // Draws lines using indices instead of coordinates
+    drawLine(row1, col1, row2, col2) {
+        line(margin + (col1 * offset), margin + (row1 * offset), 
+             margin + (col2 * offset), margin + (row2 * offset));
+    }
+}
---
+++ move.js
@@ -0,0 +1,253 @@
+class Move {
+    constructor() {
+        this.player = 1;
+        this.moveIsHappening = false;
+        this.noMovesExist = false;
+        this.chooseDir = false;
+
+        this.history = [];
+        this.verifiedMoves = []
+    }
+    
+    startNewMove(player, r, c) {
+        this.player = player;
+        this.moveIsHappening = true;
+        this.history.push([r, c]);
+        board.arrows = [];
+
+        this.checkIfMovesExist();
+
+        if (this.noMovesExist) {
+            this.findAny();
+        }
+        else {
+            this.findNext();
+        }
+    }
+
+    findAny() {
+        console.log("find any")
+        let cur = this.history[this.history.length - 1];
+        let unverifiedMoves = this.getUnverifiedMoves(cur);
+        this.verifiedMoves = unverifiedMoves;
+
+        board.highlighting = [];
+
+        this.select(cur[0], cur[1]);
+
+        this.verifiedMoves.forEach(move => {
+            board.addHighlight(move);
+        });
+    }
+
+    findNext() {
+        console.log("find normally")
+        let cur = this.history[this.history.length - 1];
+        let unverifiedMoves = this.getUnverifiedMoves(cur);
+        this.verifiedMoves = [];
+        this.getVerifiedMoves(cur[0], cur[1], unverifiedMoves);
+        
+        board.highlighting = [];
+
+        // No possible moves left
+        if (this.verifiedMoves.length == 0) {
+            // If your first selection didn't have any moves, don't end turn
+            if (this.history.length == 1) {
+                this.resetMove();
+                return;
+            }
+            
+            // Otherwise, end turn;
+            this.endMove();
+            return;
+        }
+
+        this.select(cur[0], cur[1]);
+
+        this.verifiedMoves.forEach(move => {
+            board.addHighlight(move);
+        });
+    }
+
+    nextMove(r, c) {
+        let prevPos = this.history[this.history.length - 1];
+        let dir = getDirection(prevPos[0], prevPos[1], r, c);
+        
+        // Unselect piece
+        if (r == prevPos[0] && c == prevPos[1]) {
+            this.resetMove();
+            return;
+        }
+        // Pick from two if moving towards and away;
+        else if (this.choosDir) {
+            console.log("choose two");
+            this.choosDir = false;
+        }
+         if (includes(this.verifiedMoves, [r, c])) {
+            // Swap board pieces
+            board.board[r][c] = this.player;
+            board.board[prevPos[0]][prevPos[1]] = 0;
+
+            board.addArrow(prevPos[0], prevPos[1], r, c);
+
+            this.handleTaking(r, c, dir);
+            this.history.push([r, c]);
+        }
+
+        // If no moves exist, make the one turn and finish move
+        if (this.noMovesExist) {
+            this.endMove();
+        }
+        else {
+            this.findNext();
+        }
+    }
+
+    resetMove() {
+        this.history = [];
+        board.highlighting = [];
+        this.moveIsHappening = false;
+        this.noMovesExist = false;
+        this.chooseDir = false;
+        this.deselect();
+    }
+
+    endMove() {
+        this.resetMove();
+
+        let text = document.getElementById('turn');
+
+        if (this.player == 1) {
+            this.player = 2;
+            board.p1Turn = false;
+            text.innerText = "Player 2's turn";
+            text.setAttribute('class', 'green');
+        }
+        else {
+            this.player = 1;
+            board.p1Turn = true;
+            text.innerText = "Player 1's turn";
+            text.setAttribute('class', 'beige');
+        }
+    }
+
+    handleTaking(r, c, dir) {
+        if (this.movingTowards(r, c, dir)) {
+            if (this.movingAway(r, c, dir)) {
+                // Moving towards and away... choose
+                console.log("choose two");
+                this.choosDir = true;
+                board.addHighlight([r + dir[0], c + dir[1]]);
+                board.addHighlight([r - (2 * dir[0]), c - (2 * dir[1])]);
+            }
+            else {
+                // Moving towards;
+                this.take(r, c, dir);
+            }
+        }
+        else {
+            // Moving away
+            dir = [-dir[0], -dir[1]];
+            this.take(r + dir[0], c + dir[1], dir);
+        }
+    }
+
+    take(r, c, dir) {
+        let opp = (this.player % 2) + 1;
+        r += dir[0];
+        c += dir[1];
+
+        while ((0 <= r && r < board.board.length) && (0 <= c && c < board.board[0].length)) {
+            if (board.board[r][c] == opp) {
+                board.board[r][c] = 0;
+            }
+            else break;
+            r += dir[0];
+            c += dir[1];
+        }
+    }
+
+    checkIfMovesExist() {
+        this.verifiedMoves = [];
+        for (let r = 0; r < board.board.length; r++) {
+            for (let c = 0; c < board.board[0].length; c++) {
+                if (board.board[r][c] == this.player) {
+                    let unverifiedMoves = this.getUnverifiedMoves([r, c]);
+                    this.getVerifiedMoves(r, c, unverifiedMoves);
+                }
+            }
+        }
+        if (this.verifiedMoves.length == 0) {
+            this.noMovesExist = true;
+        }
+    }
+
+    // Misc ----------------------------------------------------------------------------------
+    movingTowards(r, c, dir) {
+        r += dir[0];
+        c += dir[1]
+        if ((0 <= r && r < board.board.length) && (0 <= c && c < board.board[0].length))
+            return (board.board[r][c] == ((this.player % 2) + 1));
+        else return false;
+    }
+
+    movingAway(r, c, dir) {
+        r -= dir[0];
+        c -= dir[1]
+        if ((0 <= r && r < board.board.length) && (0 <= c && c < board.board[0].length))
+            return (board.board[r][c] == ((this.player % 2) + 1));
+        else return false;
+    }
+    
+    select(r, c) {
+        board.selected = [r, c];
+    }
+
+    deselect() {
+        board.selected = [];
+    }
+    
+    getVerifiedMoves(r, c, unverifiedMoves) {
+        // this.verifiedMoves = []
+        unverifiedMoves.forEach(move => {
+            let dir = getDirection(r, c, move[0], move[1]);
+            let opponent = (this.player % 2) + 1;
+
+            let behindR = move[0] - (2 * dir[0]);
+            let behindC = move[1] - (2 * dir[1]);
+
+            if (board.clickedOn(move[0] + dir[0], move[1] + dir[1]) == opponent 
+                || board.clickedOn(behindR, behindC) == opponent) {
+                if (!includes(this.history, [move[0], move[1]])) {
+                    this.verifiedMoves.push([move[0], move[1]]);
+                }
+            } 
+        });
+    }
+
+    getUnverifiedMoves(i) {
+        let r = i[0];
+        let c = i[1];
+        let moveType = (r + c) % 2; // 0 = 8 ways; 1 = 4 ways;
+        let unverifiedMoves = [];
+
+        if (moveType == 0) {
+            for (let row = r - 1; row <= r + 1; row++) {
+                for (let col = c - 1; col <= c + 1; col++) {
+                    if ((0 <= row && row < board.board.length) && (0 <= col && col < board.board[0].length)) {
+                        if (board.clickedOn(row, col) == 0) {
+                            unverifiedMoves.push([row, col]);
+                        }
+                    }
+                }
+            }
+        }
+        else {
+            if (r - 1 >= 0                     && board.board[r - 1][c] == 0) { unverifiedMoves.push([r - 1, c]); }
+            if (r + 1 < board.board.length    && board.board[r + 1][c] == 0) { unverifiedMoves.push([r + 1, c]); }
+            if (c - 1 >= 0                     && board.board[r][c - 1] == 0) { unverifiedMoves.push([r, c - 1]); }
+            if (c + 1 < board.board[0].length && board.board[r][c + 1] == 0) { unverifiedMoves.push([r, c + 1]); }
+        }
+        return unverifiedMoves;
+    }
+}
